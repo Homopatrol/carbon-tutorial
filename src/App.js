6109736fd1e976e04eb97ff076de07f52d6395dd
PH <PandoraH@protonmail.com>
2020-10-06 15:32:03 +0100

    feat(tutorial): complete step 4

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
-import { Button } from 'carbon-components-react';
-import './app.scss';
+import './App.scss';
+import { Button, Content } from 'carbon-components-react';
 import TutorialHeader from './components/TutorialHeader';
 import { Route, Switch } from 'react-router-dom';
 import LandingPage from './content/LandingPage';
@@ -11,15 +11,19 @@ import RepoPage from './content/RepoPage';
 class App extends Component {
   render() {
     return (
-      <>
-        <Switch>
-  <Route exact path="/" component={LandingPage} />
-  <Route path="/repos" component={RepoPage} />
-</Switch>
-      </>
-    )
+      <div className="App">
+        
+        <TutorialHeader />
+        <Content>
+         <Switch>
+            <Route exact path="/" component={LandingPage} />
+            <Route path="/repos" component={RepoPage} />
+          
+          </Switch>
+        </Content>
+      </div>
+    );
   }
 }
-
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
+import './index.scss';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 import { HashRouter as Router } from 'react-router-dom';
@@ -19,12 +19,11 @@ const client = new ApolloClient({
     },
   });
 
-ReactDOM.render(
+ReactDOM.render(<ApolloProvider client={client}>
     <Router>
       <App />
-    </Router>,
-    document.getElementById('root')
-  );
+    </Router>
+  </ApolloProvider>, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
